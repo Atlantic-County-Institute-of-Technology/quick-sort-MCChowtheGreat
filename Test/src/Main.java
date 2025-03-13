3d6191f23a42a276848bf8f9b7fd5579eeb2d612
MCChowtheGreat <chow8801@student.acitech.org>
2025-03-13 19:09:22 -0400

    3/13/25

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,10 +1,13 @@
+import java.util.Random;
+
 public class Main {
-    int partition(int a[], int low, int high){
+    int partition(int[] a, int low, int high){
         int pivot = a[high];
 
 
     }
     public static void main(String[] args) {
-
+        Random rand = new Random();
+        int[] arr = rand
     }
 }
