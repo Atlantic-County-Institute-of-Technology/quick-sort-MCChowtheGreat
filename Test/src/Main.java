b5558c2db123d155a9f25eb824879bfc99914f5e
MCChowtheGreat <chow8801@student.acitech.org>
2025-03-14 14:22:36 -0400

    3/14/25
    -Working on random generating.

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -8,6 +8,7 @@ public class Main {
     }
     public static void main(String[] args) {
         Random rand = new Random();
-        int[] arr = rand
+        int size = rand.nextInt(10);
+        int[] arr = rand.
     }
 }
